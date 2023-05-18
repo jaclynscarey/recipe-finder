332b721305853915f93ed3b34e1c7f3a5887aafa
Jaclyn Carey <jsoyama@gmail.com>
2023-05-18 01:56:12 -0400

    wip: style home page

--- src/pages/IntroPage/IntroPage.js
+++ src/pages/IntroPage/IntroPage.js
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+
 export default function IntroPage() {
+    const [randomDish, setRandomDish] = useState(undefined);
+
+    useEffect(() => {
+        async function getRandomRecipe() {
+            try {
+                const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+                const result = await response.json();
+                const randomMeal = result['meals'][0];
+                setRandomDish(randomMeal);
+            } catch (error) {
+                console.error("Error loading random recipe")
+            }
+        }
+
+        getRandomRecipe();
+    }, []);
+
+    if (!randomDish) {
+        return (
+            <div>
+                <img className="hourglass" src={`${process.env.PUBLIC_URL}/hourglass.png`} alt="hourglass" />
+            </div>
+        )
+    }
+
+
     return(
         <section>
-            Intro Page
+            <div className="left-div">
+                <h1>Randomly Selected Dish for YOU!</h1>
+                <h2>{randomDish['strMeal']}</h2>
+                <img className="random-img" src={randomDish['strMealThumb']} alt={randomDish['strMeal']} />
+            </div>
+            <div className="right-div">
+                <h3>Like what you see??</h3>
+                <h1>Use our 'Search' button above to find this recipe by name.</h1>
+
+            </div>
         </section>
     )
 }
