3dd14e6f946745aa4eefb4145ff69a192cdf2c13
Jaclyn Carey <jsoyama@gmail.com>
2023-05-17 23:46:17 -0400

    wip: implement dropdown menu, set conditions per category

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,41 +1,91 @@
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function SearchForm({ searchTerm, setSearchTerm, setResults, category }) {
-    const BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
-    const endpoint = {
-        "Nationality": "filter.php?a=",
-        "Ingredient": "filter.php?i=",
-        "Name": "search.php?s=",
-    }
+  const [dropDownItems, setDropDownItems] = useState([]);
+  const BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
+  const navigate = useNavigate();
+  const endpoint = {
+    "Nationality": ["filter.php?a=", "list.php?a=list", "strArea"],
+    "Ingredient": ["filter.php?i=", "list.php?i=list", "strIngredient"],
+    "Name": ["search.php?s=", "search.php?s=", "strMeal"],
+  };
+  
+  category = category || 'Name';
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    async function loadDropDownItems() {
+      try {
+        const response = await fetch(`${BASE_URL}${endpoint[category][1]}`);
+        const results = await response.json();
+        const resultsList = results['meals'];
+
+        const sortedItems = resultsList.map((obj, i) => obj[endpoint[category][2]]).sort();
+        setDropDownItems(["", ...sortedItems]);
+      } catch (error) {
+        console.error('Error loading dropdown items:', error);
+      }
+    }
     
-    function handleChange(event) {
-        setSearchTerm(event.target.value);
-    };
+    loadDropDownItems();
+  }, [category]);
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-        
-        try {
-            const response = await fetch(`${BASE_URL}${endpoint[category]}${searchTerm}`); // search by dish name
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+  }
 
-            const results = await response.json();
-            const resultsArr = results['meals'];
-            setResults(resultsArr);
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-            navigate(`/search/results/${searchTerm}`);
-
-        } catch (error) {
-            
+    try {
+      let searchValue = searchTerm;
+      
+      if (category !== 'Name') {
+        const selectedOption = event.target.querySelector('select').value;
+        if (selectedOption) {
+          searchValue = selectedOption;
         }
-    }
+      }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label><h1>Find Recipes by {category}</h1></label>
-            <input type="text" value={searchTerm} onChange={handleChange} placeholder={`Search by ${category}`} required />
-            <button type="submit">Search</button>
-        </form>
-    )
+      setSearchTerm(searchValue);
+
+      const response = await fetch(`${BASE_URL}${endpoint[category][0]}${searchValue}`);
+      const results = await response.json();
+      const resultsArr = results['meals'];
+      setResults(resultsArr);
+
+      navigate(`/search/results/${searchValue}`);
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label><h1>Find Recipes by {category}</h1></label>
+      {category === 'Nationality' ? (
+        <select>
+          {dropDownItems.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      ) : category === 'Ingredient' ? (
+        <div>
+          <input type="text" onChange={handleChange} placeholder={`Search by ${category}`} />
+          <select>
+            {dropDownItems.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
+      ) : (
+        <input type="text" onChange={handleChange} placeholder={`Search by ${category}`} required />
+      )}
+      <button type="submit">Search</button>
+    </form>
+  );
 }
