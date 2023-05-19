6a4203f85371b0fb34ffc05f1de4c49908ab08c4
Jaclyn Carey <jsoyama@gmail.com>
2023-05-18 23:26:43 -0400

    wip: style ResultsPage and Dish component

--- src/pages/ResultsPage/ResultsPage.js
+++ src/pages/ResultsPage/ResultsPage.js
@@ -3,10 +3,10 @@ import Dish from '../../components/Dish/Dish';
 
 export default function ResultsPage({ results, searchTerm, setMealResult }) {
     return (
-        <section>
-            <h1>Results for {searchTerm}</h1>
+        <section className='results-section'>
+            <h1 className='results-h1'>Results for {searchTerm}</h1>
             { results ?
-                <div className='dish-div'>
+                <div className='results-div'>
                     {results.map((meal) =>                 
                         <Dish key={meal.idMeal} dish={meal} id={meal.idMeal} setMealResult={setMealResult}/>
                     )}
