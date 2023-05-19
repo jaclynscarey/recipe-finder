26fbcb3d2677d315170e7d233d2490812b6a7da0
Jaclyn Carey <jsoyama@gmail.com>
2023-05-19 01:02:41 -0400

    wip: style recipe page

--- src/pages/RecipePage/RecipePage.js
+++ src/pages/RecipePage/RecipePage.js
@@ -11,17 +11,26 @@ export default function RecipePage({ mealResult }) {
     const measures = Object.keys(mealResult).filter(key => key.startsWith("strMeasure")).map(key => mealResult[key]);
 
     return (
-        <section>
-            <h1>{mealResult.strMeal}</h1>
-            <img src={mealResult.strMealThumb} alt={mealResult.strMeal} />
-            <p>{mealResult.strInstructions}</p>
-            <ul>
-                {ingredients.map(ingredient =>
-                <li>{ingredient}</li>                   
-                )}
-            </ul>
-            
-            <div>
+        <section className="recipe-section">
+            <h1 className="recipe-h1">{mealResult.strMeal}</h1>
+            <img className="recipe-img" src={mealResult.strMealThumb} alt={mealResult.strMeal} />            
+            <table>
+                <thead>
+                    <tr>
+                        <th colSpan='2'>Ingredients</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ingredients.map((ingredient, index) => (
+                        <tr>
+                            <td className="measure">{measures[index]}</td>
+                            <td className="ingredient">{ingredient}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <p className="recipe-instructions">{mealResult.strInstructions}</p>                   
+            <div className="video-div">
                 <YouTube videoId={videoId} />
             </div>
 
