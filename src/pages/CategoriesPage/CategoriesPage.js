6a06614cba5eb527f9640bd176d9175eab1f991d
Jaclyn Carey <jsoyama@gmail.com>
2023-05-18 20:35:12 -0400

    wip: edit intro style

--- src/pages/CategoriesPage/CategoriesPage.js
+++ src/pages/CategoriesPage/CategoriesPage.js
@@ -7,9 +7,8 @@ export default function CategoriesPage({ setCategory, setSearchTerm }) {
     setCategory(undefined);
 
     return (
-        <section>
-            <h1>Search by:</h1>
-            <div className="categories-container">
+        <section className="categories-section">
+            <div className="categories-div">
                 {categories.map((category) => (
                     <Category key={category} setCategory={setCategory} category={category} categories={categories} />
                 ))}
--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -2,7 +2,7 @@ import SearchForm from "../../components/SearchForm/SearchForm";
 
 export default function SearchPage({ searchTerm, setSearchTerm, setResults, category }) {
     return (
-        <section>
+        <section className="search-section">
             <SearchForm category={category} searchTerm={searchTerm} setSearchTerm={setSearchTerm} setResults={setResults} />
         </section>
     )
