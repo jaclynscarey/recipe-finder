c389bd0de512eefbcd7ceb17b1095b73f75da2f4
Jaclyn Carey <jsoyama@gmail.com>
2025-04-06 20:15:17 -1000

    add allowedOrigins to setup CORS

--- server/index.js
+++ server/index.js
@@ -6,9 +6,18 @@ require("dotenv").config();
 const reviewRoutes = require("./routes/reviews");
 
 const app = express();
+const allowedOrigins = ["http://localhost:3000", "https://recipe-finder-enhancement3.netlify.app"];
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+}));
 app.use(express.json());
 
 // Routes
@@ -16,8 +25,6 @@ app.use("/api/reviews", reviewRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
 })
 .then(() => {
     console.log("Connected to MongoDB");
